344867f4f4d530180e6b4b7eb5bc2b9c7b6288b2
Lisa Liu <liulisa@google.com>
2020-07-15 19:02:07 +0800

    spacewar: Enable LZ4 compression for ramdisks
     * This shortens the time to unpack initramfs.
    
    Change-Id: Id23daba43ae7a6003cbde29480d2f433f344df73
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_SOURCE := kernel/nothing/spacewar
