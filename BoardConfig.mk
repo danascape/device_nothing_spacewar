c5eeafd9cb928dec6666c352093069573128ce43
danascape <danascape@gmail.com>
2021-09-06 06:57:45 +0200

    spacewar: Add partition flags
    Change-Id: Ic84707ba87915a60ce57babf2596e230d0abf10b
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,12 @@ TARGET_2ND_CPU_VARIANT := cortex-a76
 # A/B
 AB_OTA_UPDATER := true
 
+AB_OTA_PARTITIONS += \
+    boot \
+    product \
+    system \
+    system_ext
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := true
@@ -45,6 +51,48 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_SOURCE := kernel/nothing/spacewar
+
+# Partitions - Metadata
+BOARD_USES_METADATA_PARTITION := true
+
+# Partitions - Boot
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Partitions - ODM
+TARGET_COPY_OUT_ODM := odm
+
+# Partitions - Spacewar
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 6438256640
+
+# Partitions - Product
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+TARGET_COPY_OUT_PRODUCT := product
+
+# Partitions - Super
+BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
+BOARD_SUPER_PARTITION_SIZE := 6438256640
+
+# Partitions - System
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Partitions - System_ext
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+
+# Partitions - Userdata
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 242889863168
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Partitions - Vendor
+BUILD_WITHOUT_VENDOR := true
+
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
