3dd72adbb36c255d04e7df74fd78bfc453b38afe
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    spacewar: Update to Boot Control HAL v1.2
    Change-Id: I26da6b732fdce8c69c4390effe3d7b727d71adac
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -33,9 +33,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
